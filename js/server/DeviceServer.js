df8ea1d30a6d595e6afc6947368272b9961b8c1c
ZachBergh <zach@aeris.io>
2016-06-30 17:45:52 +0800

    test

--- js/server/DeviceServer.js
+++ js/server/DeviceServer.js
@@ -248,6 +248,7 @@ DeviceServer.prototype = {
                             //logger.log("Session ended for " + core._connection_key);
                             delete _cores[key];
                             delete memo._allCoresByID[coreid];
+                            delete memo._attribsByID[coreid];
 
                             if (global.publisher) {
                                 global.publisher.publish(
