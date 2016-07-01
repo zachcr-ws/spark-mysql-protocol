a4fe062a14250f045ca69d407738304ee6ad52ec
ZachBergh <zach@aeris.io>
2016-07-01 14:38:42 +0800

    new version

--- js/server/main.js
+++ js/server/main.js
@@ -1,24 +1,24 @@
 /*
-*	Copyright (c) 2015 Particle Industries, Inc.  All rights reserved.
-*
-*	This program is free software; you can redistribute it and/or
-*	modify it under the terms of the GNU Lesser General Public
-*	License as published by the Free Software Foundation, either
-*	version 3 of the License, or (at your option) any later version.
-*
-*	This program is distributed in the hope that it will be useful,
-*	but WITHOUT ANY WARRANTY; without even the implied warranty of
-*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-*	Lesser General Public License for more details.
-*
-*	You should have received a copy of the GNU Lesser General Public
-*	License along with this program; if not, see <http://www.gnu.org/licenses/>.
-*/
+ *	Copyright (c) 2015 Particle Industries, Inc.  All rights reserved.
+ *
+ *	This program is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU Lesser General Public
+ *	License as published by the Free Software Foundation, either
+ *	version 3 of the License, or (at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *	Lesser General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Lesser General Public
+ *	License along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
 
 //
 //  Not the best way to deal with errors I'm told, but should be fine on a home server
 //
-process.on('uncaughtException', function (ex) {
+process.on('uncaughtException', function(ex) {
     var stack = (ex && ex.stack) ? ex.stack : "";
     logger.error('Caught exception: ' + ex + ' stack: ' + stack);
 });
@@ -26,5 +26,6 @@ process.on('uncaughtException', function (ex) {
 
 var DeviceServer = require('./DeviceServer.js');
 var server = new DeviceServer();
-server.start();
-
+setTimeout(function() {
+    server.start();
+}, 1000);
